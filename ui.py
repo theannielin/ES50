882a4c7a54e7b6632d2bb8e9898f1fb3581c7f3f
theannielin <annielin@college.harvard.edu>
2016-12-06 18:39:36 -0500

    added more buttons and can clear

--- ui.py
+++ ui.py
@@ -14,10 +14,13 @@ frame=Frame(root)
 frame.grid(row=0, column=0, sticky=N+S+E+W)
 
 matrix = [[0 for i in range(13)] for j in range(13)]
+buttons = []
 
 class MyButton:
     def __init__(self, x, y):
         self.btn =  Button(frame, text = "X", command=lambda row=x, column=y: self.toggle_text(row, column))
+        self.xval = x
+        self.yval = y
 
     def toggle_text(self, row, column):
         """toggle button text between R and G and B"""
@@ -50,28 +53,59 @@ class MyButton:
             self.btn["highlightbackground"] ="white"
             matrix[row][column] = 0
 
-class OKButton:
-    def __init__(self):
-        self.btn = Button(frame, text = "SEND",command=self.printMatrix)
+def sendInfo():
+    print matrix
+    ser = serial.Serial(serial_port, serial_speed, timeout=1)
+    string = ''
+    new_strings = []
+    for i in range(13):
+        for j in range(13):
+            string += str(matrix[i][j])
+    for i in range(13):
+        if i%2 == 0:
+            new_strings.append(string[13*i: 13*i+13])
+        else:
+            new_strings.append(string[13*i+12:13*i-1:-1])
+    string = "".join(new_strings)
+    ser.write(string)
+    print string
+    ser.close()
 
-    def printMatrix(self):
-    	# TODO: SEND STUFF!!!
-        print matrix
-        ser = serial.Serial(serial_port, serial_speed, timeout=1)
-        string = ''
-        new_strings = []
+class sendButton:
+    def __init__(self):
+        self.btn = Button(frame, text = "SEND",command=sendInfo)
+
+class clearButton:
+    def __init__(self):
+        self.btn = Button(frame, text = "CLEAR",command=self.clear)
+
+    def clear(self):
+        # reset matrix and buttons
         for i in range(13):
             for j in range(13):
-                string += str(matrix[i][j])
-        for i in range(13):
-            if i%2 == 0:
-                new_strings.append(string[13*i: 13*i+13])
-            else:
-                new_strings.append(string[13*i+12:13*i-1:-1])
-        string = "".join(new_strings)
-        ser.write(string)
-        print string
-        ser.close()
+                matrix[i][j] = 0
+        for button in buttons:
+            button.btn["text"] = "X"
+            button.btn["highlightbackground"] ="white"
+        sendInfo() 
+
+class rainbowButton:
+    def __init__(self):
+        self.btn = Button(frame, text = "RAINBOW",command=self.sendRainbow)
+
+    def sendRainbow(self):
+        # reset buttons and matrix
+        matrix = []
+        sendInfo()
+
+class surpriseButton:
+    def __init__(self):
+        self.btn = Button(frame, text = "SURPRISE",command=self.sendSurprise)
+
+    def sendSurprise(self):
+        # reset buttons and matrix
+        matrix = []
+        sendInfo()
 
 #Create a 13x13 (rows x columns) grid of buttons inside the frame
 for row_index in range(13):
@@ -79,8 +113,17 @@ for row_index in range(13):
     for col_index in range(13):
         myB = MyButton(row_index, col_index)
         myB.btn.grid(row=row_index, column=col_index, sticky=N+S+E+W)
+        buttons.append(myB)
 
-okButt = OKButton()
-okButt.btn.grid(row=6, column=14, sticky=N+S+E+W)
+sendButt = sendButton()
+sendButt.btn.grid(row=4, column=14, sticky=N+S+E+W)
+clearButt = clearButton()
+clearButt.btn.grid(row=5, column=14, sticky=N+S+E+W)
+
+rainbowButt = rainbowButton()
+rainbowButt.btn.grid(row=7, column=14, sticky=N+S+E+W)
+surpriseButt = surpriseButton()
+surpriseButt.btn.grid(row=8, column=14, sticky=N+S+E+W)
+
 
 root.mainloop()
