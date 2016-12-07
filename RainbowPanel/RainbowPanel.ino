cdb913a5eee4d9beb164d53ce71eff7fdebb546b
theannielin <annielin@college.harvard.edu>
2016-12-07 01:01:41 -0500

    can send strings to arduino via app now

--- RainbowPanel/RainbowPanel.ino
+++ RainbowPanel/RainbowPanel.ino
@@ -13,10 +13,139 @@
 //#define APA102_USE_FAST_GPIO
 
 #include <APA102.h>
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+// COMMON SETTINGS
+// ----------------------------------------------------------------------------------------------
+// These settings are used in both SW UART, HW UART and SPI mode
+// ----------------------------------------------------------------------------------------------
+#define BUFSIZE                        128   // Size of the read buffer for incoming data
+#define VERBOSE_MODE                   true  // If set to 'true' enables debug output
+
+
+// SOFTWARE UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins that will be used for 'SW' serial.
+// You should use this option if you are connecting the UART Friend to an UNO
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SWUART_RXD_PIN       9    // Required for software serial!
+#define BLUEFRUIT_SWUART_TXD_PIN       10   // Required for software serial!
+#define BLUEFRUIT_UART_CTS_PIN         11   // Required for software serial!
+#define BLUEFRUIT_UART_RTS_PIN         -1   // Optional, set to -1 if unused
+
+
+// HARDWARE UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the HW serial port you are using. Uncomment
+// this line if you are connecting the BLE to Leonardo/Micro or Flora
+// ----------------------------------------------------------------------------------------------
+#ifdef Serial1    // this makes it not complain on compilation if there's no Serial1
+  #define BLUEFRUIT_HWSERIAL_NAME      Serial1
+#endif
+
+
+// SHARED UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following sets the optional Mode pin, its recommended but not required
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_UART_MODE_PIN        12    // Set to -1 if unused
+
+
+// SHARED SPI SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins to use for HW and SW SPI communication.
+// SCK, MISO and MOSI should be connected to the HW SPI pins on the Uno when
+// using HW SPI.  This should be used with nRF51822 based Bluefruit LE modules
+// that use SPI (Bluefruit LE SPI Friend).
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SPI_CS               8
+#define BLUEFRUIT_SPI_IRQ              7
+#define BLUEFRUIT_SPI_RST              4    // Optional but recommended, set to -1 if unused
+
+// SOFTWARE SPI SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins to use for SW SPI communication.
+// This should be used with nRF51822 based Bluefruit LE modules that use SPI
+// (Bluefruit LE SPI Friend).
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SPI_SCK              13
+#define BLUEFRUIT_SPI_MISO             12
+#define BLUEFRUIT_SPI_MOSI             11
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+   
+                              Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                              running this at least once is a good idea.
+   
+                              When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                              Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+       
+                              Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         1
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+/*=========================================================================*/
+
+// Create the bluefruit object, either software serial...uncomment these lines
+/*
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+*/
+
+/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+// Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
+//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
 
 // Define which pins to use.
-const uint8_t dataPin = 11;
-const uint8_t clockPin = 12;
+const uint8_t dataPin = 5;
+const uint8_t clockPin = 6;
 
 // Create an object for writing to the LED strip.
 APA102<dataPin, clockPin> ledStrip;
@@ -60,6 +189,45 @@ void setup()
   Serial.begin(115200);
   pinMode(dataPin, OUTPUT);
   //Serial.write('7');
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Then Enter characters to send to Bluefruit"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set module to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("******************************"));
+
+  
 }
 
 /* Returns a reference to the color at the specified coordinates,
@@ -80,9 +248,14 @@ void loop()
 {
   uint8_t time = millis() >> 2;
   // If there's a new connection, flash all green
-  if(Serial.available()) {
+  if(Serial.available() || ble.available()) {
     // Return rainbow if string is not empty
-    String c = Serial.readString();
+    String c = "";
+    if (Serial.available()){
+      c = Serial.readString();
+    } else if (ble.available()) {
+      c = ble.readString();
+    }
     if (c.length() != 0) {
       for (uint8_t x = 0; x < ledPanelWidth; x++)
       {
@@ -117,7 +290,7 @@ void loop()
       while (true) {
         ledStrip.write(colors, ledCount, brightness);
         //Serial.println(c[ledPanelWidth*1+2]);
-        if (Serial.available())
+        if (Serial.available() || ble.available())
            break;
       }
     }
@@ -132,11 +305,12 @@ void loop()
       }
       while (true) {
         ledStrip.write(colors, ledCount, brightness);
-        if (Serial.available())
+        if (Serial.available() || ble.available())
            break;
       }
     }
-  } else {
+  } 
+  else {
       // No connection, first time, flash red
       for (uint8_t x = 0; x < ledPanelWidth; x++)
         {
